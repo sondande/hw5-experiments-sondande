889c8bdb67427767eb2d2fb1cf2437bd3d064313
Sagana Ondande <sondande@oberlin.edu>
2022-11-29 17:49:42 -0500

    Added neural network implementation

--- experiments.py
+++ experiments.py
@@ -1,12 +1,16 @@
-from sklearn.model_selection import train_test_split
+import os
 import sys
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 from pandas.api.types import is_string_dtype
 from pandas.api.types import is_numeric_dtype
-from sklearn import tree
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-import os
+
+
 """
 Preprocesses input dataset
 """
@@ -39,45 +43,114 @@ def data_preprocessing(dataset):
                 new_dataframe = pd.concat([new_dataframe, dataset[x]],axis=1)
     return new_dataframe
 
+"""
+Function to run an input dataset on Decision Tree Classifier
+"""
+def run_decision_tree_classifier(X_train, X_test, y_train, y_test):
+
+    # Create a decision tree classifier
+    clf = tree.DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=2,random_state=randomSeed)
+
+    # Train the classifier
+    clf = clf.fit(X_train, y_train)
+
+    # Predict the labels of the testing set
+    y_pred = clf.predict(X_test)
+
+    # Calculate the accuracy of the classifier
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Print the confusion matrix of the classifier
+    cm = confusion_matrix(y_test, y_pred)
+    # Make confusion matrix pandas dataframe
+    cm_df = pd.DataFrame(cm)
+    return cm_df, accuracy
+
+"""
+Function to run an input dataset on Neural Network Classifier
+"""
+
+def run_neural_network_classifier(X_train, X_test, y_train, y_test):
+    # Create a decision tree classifier
+    clf = MLPClassifier(max_iter=5000, random_state=randomSeed)
+
+     # Train the classifier
+    clf = clf.fit(X_train, y_train)
+
+    # Predict the labels of the testing set
+    y_pred = clf.predict(X_test)
+
+    # Calculate the accuracy of the classifier
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Print the confusion matrix of the classifier
+    cm = confusion_matrix(y_test, y_pred)
+    # Make confusion matrix pandas dataframe
+    cm_df = pd.DataFrame(cm)
+    return cm_df, accuracy
+
+"""
+Main part of the program
+"""
+
 try: 
     # Get random seed from command line
     randomSeed = int(sys.argv[1])
+
     # Get dataset file path from command line
-    dataset = sys.argv[2]
+    dataset = ["monks1.csv"]#,"votes.csv"]
+
     # Set size of training set
     training_set_size = 0.6
-    # Read dataset into a pandas dataframe
-    df = pd.read_csv(dataset)
-    # Get the target column
+
+
+    # Run decision tree classifier for each dataset in the list
+    for input_dataSet in dataset:
+        # Read dataset into a pandas dataframe
+        df = pd.read_csv(input_dataSet)
+            # Get the target column
     label_col = df.iloc[:,:1]
+
     # Get the features
     features = df.iloc[:,1:]
-    # Preprocess the dataset
+
+    # Preprocess the dataset with one hot encoding
     cleaned_features = data_preprocessing(features)
+
     # Combine the target column with the features
     df = pd.concat([label_col, cleaned_features], axis=1)
+
     # Shuffle the dataset and split into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(cleaned_features, label_col, test_size=training_set_size, random_state=randomSeed)
-    # Create a decision tree classifier
-    clf = tree.DecisionTreeClassifier()
-    # Train the classifier
-    clf = clf.fit(X_train, y_train)
-    # Predict the labels of the testing set
-    y_pred = clf.predict(X_test)
-    # Calculate the accuracy of the classifier
-    accuracy = accuracy_score(y_test, y_pred)
-    # Print the accuracy of the classifier
-    print("Accuracy: ", accuracy)
-    # Print the confusion matrix of the classifier
-    cm = confusion_matrix(y_test, y_pred)
-    # make confusion matrix pandas dataframe
-    cm_df = pd.DataFrame(cm)
+
+    # Flatten the target columns into a 1D array. Prevents a DataConversionWarning warning from being thrown
+    y_train = y_train.values.ravel()
+    y_test = y_test.values.ravel()
+
+    # Run decision tree classifier
+    cm_df, accuracy = run_decision_tree_classifier(X_train, X_test, y_train, y_test)
+
     # Structure name of file
-    filename = f"results-DecisionTree-{os.path.splitext(dataset)[0]}-{randomSeed}.csv"
-    print(filename)
+    filename = f"results-DecisionTree-{os.path.splitext(input_dataSet)[0]}-{randomSeed}.csv"
+
     # Save to file
     cm_df.to_csv(filename)
-    print(f"Confusion Matrix:\n{cm}")
+
+    print(f"File: {input_dataSet}\nModel: Decision Tree\nConfusion Matrix:\n{cm_df}\nAccuracy Score: {accuracy}\n")
+
+    # Run neaural network classifier
+    cm_df, accuracy = run_neural_network_classifier(X_train, X_test, y_train, y_test)
+
+    # Structure name of file
+    filename = f"results-NeauralNetwork-{os.path.splitext(input_dataSet)[0]}-{randomSeed}.csv"
+
+    # Save to file
+    cm_df.to_csv(filename)
+
+    print(f"File: {input_dataSet}\nModel: Neural Network\nConfusion Matrix:\n{cm_df}\nAccuracy Score: {accuracy}\n")
+
+
+
 except IndexError as e:
     print(f"Error. Message below:\n{e}\nPlease correct and try again.")
     exit(1)
